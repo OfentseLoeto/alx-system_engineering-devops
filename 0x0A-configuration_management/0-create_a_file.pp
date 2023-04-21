48ea1d566a02e7f9a4e4c1e3756b5a74050e049e
Ohentse Loeto <ohentseloeto610@gmail.com>
2023-04-21 06:45:58 -0700

    created a puppet file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,10 @@
+#Creates a file
+
 file { '/tmp/school':
-content => 'I love Puppet',
+ensure  => file,
+path    => '/tmp/school',
+mode    => '0744',
 owner   => 'www-data',
 group   => 'www-data',
-mode    => '0774',
+content => 'I love Puppet',
 }
