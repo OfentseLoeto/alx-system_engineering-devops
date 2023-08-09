c4fc066065cf6f80aed394a18b1e9268f8a38a0d
ofentse loeto <ohentseloeto610@gmail.com>
2023-08-08 20:22:39 -0700

    prints the titles of the first 10 hot posts listed for a given subreddit.

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""The script returns the number of subscribers
+   (not active users, total subscribers) for a given subreddit.
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    user_agent = ""
+    headers = {"User-Agent": user_agent}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            subscribers = data["data"]["subscribers"]
+            return subscribers
+        except (KeyError, ValueError):
+            return 0
+    else:
+        return 0
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""The script prints the titles of the first 10
+   hot posts listed for a given subreddit.
+"""
+
+import requests
+
+def top_ten(subreddit):
+    user_agent = ""
+    headers = {"User-Agent": user_agent}
+    url = f"https://www.reddis.com/r/{subreddit}/hot.json"
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        """Raise an exception for 4xx and 5xx status codes"""
+        response.raise_for_status()
+        data = response.json()
+
+        if "data" in data and "children" in data["data"]:
+            """Take the first 10 posts"""
+            posts = data["data"]["children"][:10]
+
+            for posts in posts:
+                print(posts["data"]["title"])
+        else:
+            print("No posts found")
+
+    except (requests.RequestException, KeyError, ValueError):
+        print("None")
