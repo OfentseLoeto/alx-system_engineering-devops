2061f6a24a0c5c48e7bc6ecc3cf3b4b00cea768d
ohentse loeto <ohentseloeto610@gmail.com>
2023-08-24 12:37:14 +0000

    find out why Apache is returning a 500 error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,31 +1,6 @@
-# Fix error 500
-class apache_fix {
- package { 'apache2':
- ensure => installed,
-}
-
-file { '/var/www/html/wp-settings.php':
- ensure  => file,
- source  => 'puppet:///modules/apache_fix/wp-settings.php',
- mode    => '0644',
- require => Package['apache2'],
- notify  => Exec['fix-php-500'],
-}
-
+# fix error 500
 exec { 'fix-php-500':
- command     => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
- path        => ['/usr/bin', '/bin'],
- returns     => [0, 1],
- require     => File['/var/www/html/wp-settings.php'],
- notify      => Exec['restart_apache'],
+  command  => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
+  path     => ['/usr/bin', '/bin'],
+  returns  => [0, 1],
 }
-
-exec { 'restart_apache':
- command     => '/usr/sbin/service apache2 restart',
- refreshonly => true,
- subscribe   => File['/var/www/html/wp-settings.php'],
- require     => Package['apache2'],
-}
-}
-
-class { 'apache_fix': }
